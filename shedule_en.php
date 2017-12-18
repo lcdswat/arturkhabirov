baa0bc4f0897f1624800328e46d1ab300b3621d8
Marat Bakirov <lcdswat@debian-web>
2017-12-18 03:59:57 -0600

    Поправил расписание

--- shedule_en.php
+++ shedule_en.php
@@ -1,8 +1,8 @@
 ﻿<span class="text-21">CURRENTLY YOU CAN VISIT MY DANCE CLASSES IN UFA AT:<br>
 MENDELEEV ST., 205А (SHOPPING CENTER "BASHKORTOSTAN"), CREATIVE SPACES "FACTURY"<br><br>
-WEDNESDAY, FRIDAY: 10:00—11:30&nbsp;&nbsp;|&nbsp;&nbsp;19:30—21:00<br />
-TUESDAY, THURSDAY: 10:00—11:30&nbsp;&nbsp;|&nbsp;&nbsp;16:00—17:30&nbsp;&nbsp;|&nbsp;&nbsp;21:00—22:30<br />
- SUNDAY: 13:30—15:00<br />
+<!-- WEDNESDAY, FRIDAY: 10:00—11:30&nbsp;&nbsp;|&nbsp;&nbsp;19:30—21:00<br /> -->
+TUESDAY, THURSDAY: <!-- 10:00—11:30&nbsp;&nbsp;|&nbsp;&nbsp;16:00—17:30&nbsp;&nbsp;|&nbsp;&nbsp; -->21:00—22:30<br />
+ <!-- SUNDAY: 13:30—15:00<br /> -->
 </span>
 
 <div class="map1">
